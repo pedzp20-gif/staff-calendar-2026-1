3a005f837835135fde6ef1e47804606f4da641f8
pedzp20-gif <pedzp20@gmail.com>
2026-01-29 20:28:38 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,7 @@
+const CACHE="staff-calendar-realtime-v1";
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["index.html","manifest.json"])));
+});
 self.addEventListener("fetch",e=>{
- e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
